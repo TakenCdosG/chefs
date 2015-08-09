8fdf5c4313a96bb52f25fe95abac0e2786d79c1b
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-09 17:25:52 -0500

    Ajustes Stilos

--- wp-content/themes/travelify/header.php
+++ wp-content/themes/travelify/header.php
@@ -6,72 +6,74 @@
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
+    <head>
 
-	<?php
-		/**
-		 * travelify_meta hook
-		 */
-		do_action( 'travelify_meta' );
+        <?php
+        /**
+         * travelify_meta hook
+         */
+        do_action('travelify_meta');
 
-		/**
-		 * travelify_links hook
-		 *
-		 * HOOKED_FUNCTION_NAME PRIORITY
-		 *
-		 * travelify_add_links 10
-		 * travelify_favicon 15
-		 * travelify_webpageicon 20
-		 *
-		 */
-		do_action( 'travelify_links' );
+        /**
+         * travelify_links hook
+         *
+         * HOOKED_FUNCTION_NAME PRIORITY
+         *
+         * travelify_add_links 10
+         * travelify_favicon 15
+         * travelify_webpageicon 20
+         *
+         */
+        do_action('travelify_links');
+        ?>
+        <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+        <?php
+        /**
+         * This hook is important for WordPress plugins and other many things
+         */
+        wp_head();
+        ?>
 
-		/**
-		 * This hook is important for WordPress plugins and other many things
-		 */
-		wp_head();
-	?>
+    </head>
 
-</head>
+    <body <?php body_class(); ?>>
+        <?php
+        /**
+         * travelify_before hook
+         */
+        do_action('travelify_before');
+        ?>
 
-<body <?php body_class(); ?>>
-	<?php
-		/**
-		 * travelify_before hook
-		 */
-		do_action( 'travelify_before' );
-	?>
+        <div class="wrapper">
+            <?php
+            /**
+             * travelify_before_header hook
+             */
+            do_action('travelify_before_header');
+            ?>
+            <header id="branding" >
+                <?php
+                /**
+                 * travelify_header hook
+                 *
+                 * HOOKED_FUNCTION_NAME PRIORITY
+                 *
+                 * travelify_headerdetails 10
+                 */
+                do_action('travelify_header');
+                ?>
+            </header>
+            <?php
+            /**
+             * travelify_after_header hook
+             */
+            do_action('travelify_after_header');
+            ?>
 
-	<div class="wrapper">
-		<?php
-			/**
-			 * travelify_before_header hook
-			 */
-			do_action( 'travelify_before_header' );
-		?>
-		<header id="branding" >
-			<?php
-				/**
-				 * travelify_header hook
-				 *
-				 * HOOKED_FUNCTION_NAME PRIORITY
-				 *
-				 * travelify_headerdetails 10
-				 */
-				do_action( 'travelify_header' );
-			?>
-		</header>
-		<?php
-			/**
-			 * travelify_after_header hook
-			 */
-			do_action( 'travelify_after_header' );
-		?>
-
-		<?php
-			/**
-			 * travelify_before_main hook
-			 */
-			do_action( 'travelify_before_main' );
-		?>
-		<div id="main" class="container clearfix">
+            <?php
+            /**
+             * travelify_before_main hook
+             */
+            do_action('travelify_before_main');
+            ?>
+            <div id="main" class="container clearfix">
