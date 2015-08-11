6a11babfd11a9c2025bf127b49ab84d7b34614ce
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-11 12:49:57 -0500

    Ajustes Seo

--- wp-content/themes/travelify/templates/template-front-page.php
+++ wp-content/themes/travelify/templates/template-front-page.php
@@ -16,6 +16,21 @@
 do_action('travelify_before_main_container');
 $text_top = get_field("text_top");
 $title_top = get_field("title_top");
+
+// Boxes.
+// Left.
+$boxes_left_background_image = get_field("boxes_left_background_image");
+$boxes_left_title_red = get_field("boxes_left_title_red");
+$boxes_left_title_black = get_field("boxes_left_title_black");
+$boxes_left_link_text = get_field("boxes_left_link_text");
+$boxes_left_link_url = get_field("boxes_left_link_url");
+
+// Right.
+$boxes_right_background_image = get_field("boxes_right_background_image");
+$boxes_right_title_red = get_field("boxes_right_title_red");
+$boxes_right_title_black = get_field("boxes_right_title_black");
+$boxes_right_link_text = get_field("boxes_right_link_text");
+$boxes_right_link_url = get_field("boxes_right_link_url");
 ?>
 
 <div id="container">
@@ -27,6 +42,54 @@ $title_top = get_field("title_top");
     <?php if (!empty($title_top)): ?>
         <div class="title_top">
             <h1><?php echo $title_top; ?></h1>
+        </div>
+    <?php endif; ?>
+    <?php if (!empty($boxes_left_background_image) && !empty($boxes_right_background_image)): ?>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="box">
+                    <?php if (!empty($boxes_left_background_image)): ?>
+                        <img width="" height="" src="<?php echo $boxes_left_background_image; ?>" class="img-responsive" alt="<?php echo $boxes_left_title_red . " " . $boxes_left_title_black; ?>" title="<?php echo $boxes_left_title_red . " " . $boxes_left_title_black; ?>">
+                    <?php endif; ?>
+                    <div class="box-summary-right">
+                        <?php if (!empty($boxes_left_title_red)): ?>
+                            <h1 class="red-title">
+                                <?php echo $boxes_left_title_red; ?>
+                            </h1>
+                        <?php endif; ?>
+                        <?php if (!empty($boxes_left_title_black)): ?>
+                            <h3 class="black-title">
+                                <?php echo $boxes_left_title_black; ?>
+                            </h3>
+                        <?php endif; ?>
+                        <?php if (!empty($boxes_left_link_url) && !empty($boxes_left_link_text)): ?>
+                            <a class="box-link-red" href="<?php echo $boxes_left_link_url; ?>"><?php echo $boxes_left_link_text; ?></a>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="box">
+                    <?php if (!empty($boxes_right_background_image)): ?>
+                        <img width="" height="" src="<?php echo $boxes_right_background_image; ?>" class="img-responsive" alt="<?php echo $boxes_right_title_red . " " . $boxes_right_title_black; ?>" title="<?php echo $boxes_right_title_red . " " . $boxes_right_title_black; ?>">
+                    <?php endif; ?>
+                    <div class="box-summary-right">
+                        <?php if (!empty($boxes_right_title_red)): ?>
+                            <h1 class="red-title">
+                                <?php echo $boxes_right_title_red; ?>
+                            </h1>
+                        <?php endif; ?>
+                        <?php if (!empty($boxes_right_title_black)): ?>
+                            <h3 class="black-title">
+                                <?php echo $boxes_right_title_black; ?>
+                            </h3>
+                        <?php endif; ?>
+                        <?php if (!empty($boxes_right_link_url) && !empty($boxes_right_link_text)): ?>
+                            <a class="box-link-red" href="<?php echo $boxes_right_link_url; ?>"><?php echo $boxes_right_link_text; ?></a>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
         </div>
     <?php endif; ?>
     <?php
