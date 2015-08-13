000b467b1fd4a789555106b98b6d500e138c8707
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-13 10:54:41 -0500

    Ajustes en las librerias del tema

--- wp-content/themes/travelify/library/js/global.js
+++ wp-content/themes/travelify/library/js/global.js
@@ -14,6 +14,8 @@ jQuery(function ($) {
             itemMargin: 5,
             controlNav: false, //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
             directionNav: true, //Boolean: Create navigation for previous/next navigation? (true/false)
+            prevText: "", //String: Set the text for the "previous" directionNav item
+            nextText: "",
         });
     });
     function initialize() {
