cf8cc3441645bd8e855c3f0170462d6415d2d7cf
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-13 11:03:24 -0500

    Ajustes en las librerias del tema

--- wp-content/themes/travelify/library/js/global.js
+++ wp-content/themes/travelify/library/js/global.js
@@ -12,7 +12,7 @@ jQuery(function ($) {
             animationLoop: false,
             itemWidth: 194,
             itemMargin: 5,
-            controlNav: false, //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
+            controlNav: true, //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
             directionNav: true, //Boolean: Create navigation for previous/next navigation? (true/false)
             prevText: "", //String: Set the text for the "previous" directionNav item
             nextText: "",
