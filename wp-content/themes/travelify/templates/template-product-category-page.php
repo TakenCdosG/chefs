6a31c62cbae9ba5528c6463bdb4bd30757fcd47d
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-15 16:03:21 -0500

    WooCommerce Templates.

--- wp-content/themes/travelify/templates/template-product-category-page.php
+++ wp-content/themes/travelify/templates/template-product-category-page.php
@@ -41,7 +41,7 @@ if (count($categories) > 0) {
 $args = array(
     'post_type' => 'product',
     'posts_per_page' => 9,
-    'product_cat' => $categories,
+    'product_cat' => "21",
     'tax_query' => $tax_query
 );
 
