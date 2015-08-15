dabf739fab8c09346ef0077a7c5e75e1f30cf1f3
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-15 11:40:04 -0500

    WooCommerce Templates.

--- wp-content/themes/travelify/templates/template-product-category-page.php
+++ wp-content/themes/travelify/templates/template-product-category-page.php
@@ -48,4 +48,13 @@ do_action('travelify_before_main_container');
 do_action('travelify_after_main_container');
 ?>
 
+<?php
+/**
+ * woocommerce_after_main_content hook
+ *
+ * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+ */
+do_action('woocommerce_after_main_content');
+?>
+
 <?php get_footer(''); ?>
