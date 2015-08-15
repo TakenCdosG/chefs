bc051f369f248f6c05efca6ef863139f673066d0
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-15 17:12:11 -0500

    WooCommerce Templates.

--- wp-content/themes/travelify/woocommerce/content-product.php
+++ wp-content/themes/travelify/woocommerce/content-product.php
@@ -55,22 +55,25 @@ if (0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns']) {
          * @hooked woocommerce_template_loop_product_thumbnail - 10
          */
         do_action('woocommerce_before_shop_loop_item_title');
-
-        /**
-         * woocommerce_shop_loop_item_title hook
-         *
-         * @hooked woocommerce_template_loop_product_title - 10
-         */
-        do_action('woocommerce_shop_loop_item_title');
-
-        /**
-         * woocommerce_after_shop_loop_item_title hook
-         *
-         * @hooked woocommerce_template_loop_rating - 5
-         * @hooked woocommerce_template_loop_price - 10
-         */
-        do_action('woocommerce_after_shop_loop_item_title');
         ?>
+        <div class="product-summary">
+            <?php
+            /**
+             * woocommerce_shop_loop_item_title hook
+             *
+             * @hooked woocommerce_template_loop_product_title - 10
+             */
+            do_action('woocommerce_shop_loop_item_title');
+
+            /**
+             * woocommerce_after_shop_loop_item_title hook
+             *
+             * @hooked woocommerce_template_loop_rating - 5
+             * @hooked woocommerce_template_loop_price - 10
+             */
+            do_action('woocommerce_after_shop_loop_item_title');
+            ?>
+        </div>
 
     </a>
 
