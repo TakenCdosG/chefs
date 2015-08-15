9d997c8a522606ea9e3f71840b500a64f78d83be
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-15 11:50:29 -0500

    WooCommerce Templates.

--- wp-content/themes/travelify/templates/template-product-category-page.php
+++ wp-content/themes/travelify/templates/template-product-category-page.php
@@ -5,13 +5,9 @@
  * Displays the Product Category Page
  *
  */
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
-}
-
-get_header('shop');
 ?>
 
+<?php get_header(''); ?>
 <?php
 /**
  * woocommerce_before_main_content hook
@@ -21,49 +17,35 @@ get_header('shop');
  */
 do_action('woocommerce_before_main_content');
 ?>
+<?php
+/**
+ * travelify_before_main_container hook
+ */
+do_action('travelify_before_main_container');
+//echo "<pre>";
+//var_dump($category);
+//echo "</pre>";
+?>
 
-<?php if (apply_filters('woocommerce_show_page_title', true)) : ?>
-    <h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
-<?php endif; ?>
+<div id="container">
+    <?php
+    /**
+     * travelify_main_container hook
+     *
+     * HOOKED_FUNCTION_NAME PRIORITY
+     *
+     * travelify_content 10
+     */
+    do_action('travelify_main_container');
+    ?>
+</div><!-- #container -->
 
 <?php
 /**
- * woocommerce_archive_description hook
- * 
- * @hooked woocommerce_taxonomy_archive_description - 10
- * @hooked woocommerce_product_archive_description - 10
+ * travelify_after_main_container hook
  */
-do_action('woocommerce_archive_description');
+do_action('travelify_after_main_container');
 ?>
-
-<?php if (have_posts()) : ?>
-    <?php
-    /**
-     * woocommerce_before_shop_loop hook
-     *
-     * @hooked woocommerce_result_count - 20
-     * @hooked woocommerce_catalog_ordering - 30
-     */
-    do_action('woocommerce_before_shop_loop');
-    ?>
-    <?php woocommerce_product_loop_start(); ?>
-    <?php woocommerce_product_subcategories(); ?>
-    <?php while (have_posts()) : the_post(); ?>
-        <?php wc_get_template_part('content', 'product'); ?>
-    <?php endwhile; // end of the loop. ?>
-    <?php woocommerce_product_loop_end(); ?>
-    <?php
-    /**
-     * woocommerce_after_shop_loop hook
-     *
-     * @hooked woocommerce_pagination - 10
-     */
-    do_action('woocommerce_after_shop_loop');
-    ?>
-
-<?php elseif (!woocommerce_product_subcategories(array('before' => woocommerce_product_loop_start(false), 'after' => woocommerce_product_loop_end(false)))) : ?>
-    <?php wc_get_template('loop/no-products-found.php'); ?>
-<?php endif; ?>
 
 <?php
 /**
@@ -73,12 +55,5 @@ do_action('woocommerce_archive_description');
  */
 do_action('woocommerce_after_main_content');
 ?>
-<?php
-/**
- * woocommerce_sidebar hook
- *
- * @hooked woocommerce_get_sidebar - 10
- */
-do_action('woocommerce_sidebar');
-?>
-<?php get_footer('shop'); ?>
+
+<?php get_footer(''); ?>
