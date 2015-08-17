0597c606df9567a6fa93d8e42f70f03ac93f7845
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-17 12:45:34 -0500

    XML Sitemap tree.

--- wp-content/themes/travelify/templates/template-product-category-page.php
+++ wp-content/themes/travelify/templates/template-product-category-page.php
@@ -39,8 +39,9 @@ if (count($categories) > 0) {
 }
 
 $args_category = array(
-    //'child_of' => $product_cat_id,
+    'parent' => $product_cat_id,
     'taxonomy' => 'product_cat',
+    'hide_empty' => 0,
 );
 $categories_parent = get_categories($args_category);
 dpm($categories_parent);
