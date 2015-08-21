70d2b7afd83bb108f2b67f6e0a9d79413c4643e9
Adrián A. Morelos Henriquez <adrian.morelos@akendos.com>
2015-08-20 21:55:09 -0500

    Filtros pagina de categorias de productos.

--- wp-content/themes/travelify/templates/template-product-category-page.php
+++ wp-content/themes/travelify/templates/template-product-category-page.php
@@ -274,7 +274,7 @@ $info = array(
                     'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
                     'format' => '?paged=%#%',
                     'current' => max( 1, get_query_var('paged') ),
-                    'total' => $products ->max_num_pages;
+                    'total' => $products->max_num_pages;
                 ) );
                 ?>
             </div>
