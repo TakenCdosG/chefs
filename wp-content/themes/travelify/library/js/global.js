dddf7ffea4b87d030993c8a94723a68522e23442
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-29 22:48:52 -0500

    Pagina de Contacto.

--- wp-content/themes/travelify/library/js/global.js
+++ wp-content/themes/travelify/library/js/global.js
@@ -1,26 +1,145 @@
 /**
  * Global Js
  */
- 
 
 jQuery(function ($) {
+
+    /*
+     *  render_map
+     *
+     *  This function will render a Google Map onto the selected jQuery element
+     *
+     *  @type	function
+     *  @date	8/11/2013
+     *  @since	4.3.0
+     *
+     *  @param	$el (jQuery element)
+     *  @return	n/a
+     */
+    function render_map($el) {
+        // var
+        var $markers = $el.find('.marker');
+        // vars
+        var args = {
+            zoom		: 16,
+            center		: new google.maps.LatLng(0, 0),
+            mapTypeId	: google.maps.MapTypeId.ROADMAP
+        };
+        // create map
+        var map = new google.maps.Map( $el[0], args);
+        // add a markers reference
+        map.markers = [];
+        // add markers
+        $markers.each(function(){
+            add_marker( $(this), map );
+        });
+        // center map
+        center_map( map );
+    }
+    /*
+     *  add_marker
+     *
+     *  This function will add a marker to the selected Google Map
+     *
+     *  @type	function
+     *  @date	8/11/2013
+     *  @since	4.3.0
+     *
+     *  @param	$marker (jQuery element)
+     *  @param	map (Google Map object)
+     *  @return	n/a
+     */
+    function add_marker($marker,map) {
+        // var
+        var latlng = new google.maps.LatLng($marker.attr('data-lat'), $marker.attr('data-lng') );
+        // create marker
+        var marker = new google.maps.Marker({
+            position	: latlng,
+            map			: map
+        });
+        // add to array
+        map.markers.push(marker);
+        // if marker contains HTML, add it to an infoWindow
+        if($marker.html()){
+            // create info window
+            var infowindow = new google.maps.InfoWindow({
+                content		: $marker.html()
+            });
+            // show info window when marker is clicked
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.open( map, marker );
+            });
+        }
+    }
+    /*
+     *  center_map
+     *
+     *  This function will center the map, showing all markers attached to this map
+     *
+     *  @type	function
+     *  @date	8/11/2013
+     *  @since	4.3.0
+     *
+     *  @param	map (Google Map object)
+     *  @return	n/a
+     */
+    function center_map( map ) {
+        // vars
+        var bounds = new google.maps.LatLngBounds();
+        // loop through all markers and create bounds
+        $.each( map.markers, function( i, marker ){
+            var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
+            bounds.extend( latlng );
+        });
+        // only 1 marker?
+        if( map.markers.length == 1 )
+        {
+            // set center of map
+            map.setCenter( bounds.getCenter() );
+            map.setZoom( 16 );
+        }
+        else
+        {
+            // fit to bounds
+            map.fitBounds( bounds );
+        }
+
+    }
+
+    /*
+     *  document ready
+     *
+     *  This function will render each map when the document is ready (page has loaded)
+     *
+     *  @type	function
+     *  @date	8/11/2013
+     *  @since	5.0.0
+     *
+     *  @param	n/a
+     *  @return	n/a
+     */
+
     $(document).ready(function () {
         initialize();
         $('ul.products').infinitescroll({
-            navSelector  : "div.pagination-category",            
+            navSelector  : "div.pagination-category",
             // selector for the paged navigation (it will be hidden)
-            nextSelector : "div.pagination-category a:first",    
+            nextSelector : "div.pagination-category a:first",
             // selector for the NEXT link (to page 2)
-            itemSelector : "ul.products li.product",          
+            itemSelector : "ul.products li.product",
             // selector for all items you'll retrieve
-            debug : true,                        
+            debug : true,
             // enable debug messaging ( to console.log )
-            loadingImg   : "/img/loading.gif",          
+            loadingImg   : "/img/loading.gif",
             // loading image.
-            loadingText  : "Loading new posts...",   
+            loadingText  : "Loading new posts...",
             donetext     : "You've reached the end of the products for this category.",
-            loadingText  : "Loading new products...", 
-            animate      : false,   
+            loadingText  : "Loading new products...",
+            animate      : false
+        });
+
+        $('.acf-map').each(function(){
+            render_map( $(this) );
         });
     }); // End doc ready
     $(window).load(function () {
@@ -33,8 +152,8 @@ jQuery(function ($) {
             directionNav: true, //Boolean: Create navigation for previous/next navigation? (true/false)
             prevText: "", //String: Set the text for the "previous" directionNav item
             nextText: "",
-            animationLoop: true, 
-            move: 1, 
+            animationLoop: true,
+            move: 1
         });
     });
     function initialize() {
