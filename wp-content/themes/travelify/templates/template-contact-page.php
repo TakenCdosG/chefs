4abd96fb97d9cabdf56db7c9b0abb8f38ef54fe4
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-29 22:35:40 -0500

    Pagina de Contacto.

--- wp-content/themes/travelify/templates/template-contact-page.php
+++ wp-content/themes/travelify/templates/template-contact-page.php
@@ -14,20 +14,28 @@
  * travelify_before_main_container hook
  */
 do_action('travelify_before_main_container');
+$contact_map = get_field("map");
 ?>
 
-<div id="container">
-    <?php
-    /**
-     * travelify_main_container hook
-     *
-     * HOOKED_FUNCTION_NAME PRIORITY
-     *
-     * travelify_content 10
-     */
-    do_action('travelify_main_container');
-    ?>
-</div><!-- #container -->
+    <div id="container">
+        <div class="row margin-grid">
+            <div class="col-md-3">
+                <?php
+                /**
+                 * travelify_main_container hook
+                 *
+                 * HOOKED_FUNCTION_NAME PRIORITY
+                 *
+                 * travelify_content 10
+                 */
+                do_action('travelify_main_container');
+                ?>
+            </div>
+            <div class="col-md-9">
+                <?php echo $contact_map; ?>
+            </div>
+        </div>
+    </div><!-- #container -->
 
 <?php
 /**
