7c69cc2344ecdc24bf4738caf8a25bfbe163af88
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-31 09:20:59 -0500

    Ajustes Google Map.

--- wp-content/themes/travelify/library/js/global.js
+++ wp-content/themes/travelify/library/js/global.js
@@ -157,7 +157,7 @@ jQuery(function ($) {
         });
     });
     function initialize() {
-        var myLatlngMapCenter = new google.maps.LatLng(40.758023, -73.969549);
+        var myLatlngMapCenter = new google.maps.LatLng(41.257697, -73.013472);
         var myLatlngMarkerCenter = new google.maps.LatLng(40.7585431, -73.9697777);
         var myOptions = {
             zoom: 12,
@@ -168,12 +168,12 @@ jQuery(function ($) {
 
         var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
         var infowindow = new google.maps.InfoWindow({
-            content: "<div style='font-family: Roboto,Arial,sans-serif;font-size: 11px;margin: 0;padding: 0;line-height: 15px;'><strong style='font-size: 11px;'>LOREM IPSUM DOLOR SIT AMET</strong><br/>880 3rd Avenue, New York, NY 10022</div>"
+            content: "<div style='font-family: Roboto,Arial,sans-serif;font-size: 11px;margin: 0;padding: 0;line-height: 15px;'><strong style='font-size: 11px;'>Chef's Emporium</strong><br/>449 Boston Post Rd, Orange, CT 06477</div>"
         });
         var marker = new google.maps.Marker({
             position: myLatlngMapCenter,
             map: map,
-            title: "LOREM IPSUM DOLOR SIT AMET"
+            title: "Chef's Emporium"
         });
         google.maps.event.addListener(marker, 'click', function () {
             infowindow.open(map, marker);
