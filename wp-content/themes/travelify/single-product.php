fd5a722bc74db9f3ef20ba040426e1b49b2a0d9a
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-08-31 15:34:40 -0500

    Pagina de Contacto.

--- wp-content/themes/travelify/single-product.php
+++ wp-content/themes/travelify/single-product.php
@@ -23,7 +23,34 @@ get_header('shop');
  * @hooked woocommerce_breadcrumb - 20
  */
 do_action('woocommerce_before_main_content');
+
+$top_banner = get_field("header_top_product");
+$header_top_summary_product = get_field("header_top_summary_product");
+$header_top_link_text_product = get_field("header_top_link_text_product");
+$header_top_link_url_product = get_field("header_top_link_url_product");
 ?>
+
+<?php if (!empty($top_banner)): ?>
+    <div class="row margin-grid">
+        <div class="col-md-12">
+            <div class="top_banner_product_category">
+                <img width="" height="" src="<?php echo $top_banner; ?>" class="img-responsive" alt="" title="">
+                <?php if (!empty($header_top_summary)): ?>
+                    <div class="header_top_summary">
+                        <h2><?php echo $header_top_summary_product; ?></h2>
+                        <?php if (!empty($header_top_link_text_product) && !empty($header_top_link_url_product)): ?>
+                            <a href="<?php echo $header_top_link_url_product; ?>"><?php echo $header_top_link_text_product; ?></a>
+                        <?php endif; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
+
+<!-- Add clearfix -->
+<div class="clearfix-block"></div>
+
 <?php while (have_posts()) : the_post(); ?>
     <?php wc_get_template_part('content', 'single-product'); ?>
 <?php endwhile; // end of the loop.   ?>
