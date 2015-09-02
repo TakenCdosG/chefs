f6fdc47d37f28328601abe799611c44d1ea72eb2
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-09-02 15:48:21 -0500

    Pagina de Contacto.

--- wp-content/themes/travelify/library/functions/functions.php
+++ wp-content/themes/travelify/library/functions/functions.php
@@ -35,11 +35,6 @@ function wooc_extra_register_fields() {
 
     <div class="clear"></div>
 
-    <p class="form-row form-row-wide">
-    <label for="reg_billing_phone"><?php _e( 'Phone', 'woocommerce' ); ?> <span class="required">*</span></label>
-    <input type="text" class="input-text" name="billing_phone" id="reg_billing_phone" value="<?php if ( ! empty( $_POST['billing_phone'] ) ) esc_attr_e( $_POST['billing_phone'] ); ?>" />
-    </p>
-
     <?php
 }
 
@@ -57,6 +52,7 @@ add_action('woocommerce_register_form_start', 'wooc_extra_register_fields' );
  * @return void
  */
 function wooc_validate_extra_register_fields( $username, $email, $validation_errors ) {
+
     if ( isset( $_POST['billing_first_name'] ) && empty( $_POST['billing_first_name'] ) ) {
         $validation_errors->add( 'billing_first_name_error', __( '<strong>Error</strong>: First name is required!', 'woocommerce' ) );
     }
@@ -65,10 +61,6 @@ function wooc_validate_extra_register_fields( $username, $email, $validation_err
         $validation_errors->add( 'billing_last_name_error', __( '<strong>Error</strong>: Last name is required!.', 'woocommerce' ) );
     }
 
-
-    if ( isset( $_POST['billing_phone'] ) && empty( $_POST['billing_phone'] ) ) {
-        $validation_errors->add( 'billing_phone_error', __( '<strong>Error</strong>: Phone is required!.', 'woocommerce' ) );
-    }
 }
 
 add_action( 'woocommerce_register_post', 'wooc_validate_extra_register_fields', 10, 3 );
@@ -84,6 +76,7 @@ add_action( 'woocommerce_register_post', 'wooc_validate_extra_register_fields', 
  * @return void
  */
 function wooc_save_extra_register_fields( $customer_id ) {
+
     if ( isset( $_POST['billing_first_name'] ) ) {
         // WordPress default first name field.
         update_user_meta( $customer_id, 'first_name', sanitize_text_field( $_POST['billing_first_name'] ) );
@@ -100,10 +93,6 @@ function wooc_save_extra_register_fields( $customer_id ) {
         update_user_meta( $customer_id, 'billing_last_name', sanitize_text_field( $_POST['billing_last_name'] ) );
     }
 
-    if ( isset( $_POST['billing_phone'] ) ) {
-        // WooCommerce billing phone
-        update_user_meta( $customer_id, 'billing_phone', sanitize_text_field( $_POST['billing_phone'] ) );
-    }
 }
 
 add_action( 'woocommerce_created_customer', 'wooc_save_extra_register_fields' );
