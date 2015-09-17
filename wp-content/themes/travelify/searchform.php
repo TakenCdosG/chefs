d00237c32be333f87eb42caa37de5dcd2751cb9d
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-09-17 14:20:24 -0500

    Pagina de Contacto.

--- wp-content/themes/travelify/searchform.php
+++ wp-content/themes/travelify/searchform.php
@@ -6,4 +6,5 @@
 	<form action="<?php echo esc_url( home_url( '/' ) ); ?>" class="searchform clearfix" method="get">
 		<label class="assistive-text" for="s"><?php _e( 'Search', 'travelify' ); ?></label>
 		<input type="text" placeholder="<?php esc_attr_e( 'Search', 'travelify' ); ?>" class="s field" name="s">
+        <input type="submit" class="field-submit .btn-search" value="">
 	</form>
