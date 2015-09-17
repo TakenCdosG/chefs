d8b66035168c8f46953b91dcda95a8b50f0748b0
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-09-17 14:48:20 -0500

    Pagina de Contacto.

--- wp-content/themes/travelify/woocommerce/myaccount/my-account.php
+++ wp-content/themes/travelify/woocommerce/myaccount/my-account.php
@@ -17,7 +17,7 @@ wc_print_notices(); ?>
 	<?php
 	printf(
 		__( 'Hello <strong>%1$s</strong> (not %1$s? <a href="%2$s">Sign out</a>).', 'woocommerce' ) . ' ',
-		$current_user->display_name,
+        ucwords($current_user->display_name),
 		wc_get_endpoint_url( 'customer-logout', '', wc_get_page_permalink( 'myaccount' ) )
 	);
 
