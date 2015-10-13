e76704cb47779e7b374ad3eef3a4f6816d0f761e
Adrián A. Morelos Henriquez <adrian.morelos@akendos.com>
2015-10-13 15:33:46 -0500

    wp_exposed_header_callback

--- wp-content/themes/travelify/exposed_header.php
+++ wp-content/themes/travelify/exposed_header.php
@@ -38,67 +38,69 @@ if (!empty($elements)) {
 
 ?>
 
-<div class="container clearfix">
-    <div class="hgroup-wrap clearfix">
-        <div class="hgroup-wrap-left">
-            <!-- #site-logo -->
-            <hgroup id="site-logo" class="clearfix">
-                <h1 id="site-title">
-                    <a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home">
-                        <img src="<?php echo $options['header_logo']; ?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
-                    </a>
-                </h1>
-            </hgroup>
-            <!-- #site-logo -->
-        </div>
-        <div class="hgroup-wrap-right">
-            <div class="menu-header-top">
-                <?php
-                $defaults = array(
-                    'menu'            => '77',
-                    'container'       => 'div',
-                    'container_class' => 'header_upper',
-                    'container_id'    => '',
-                    'menu_class'      => 'menu',
-                    'menu_id'         => '',
-                    'echo'            => true,
-                );
-                wp_nav_menu($defaults);
-                ?>
+<div class="exposed-header">
+    <div class="container clearfix">
+        <div class="hgroup-wrap clearfix">
+            <div class="hgroup-wrap-left">
+                <!-- #site-logo -->
+                <hgroup id="site-logo" class="clearfix">
+                    <h1 id="site-title">
+                        <a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home">
+                            <img src="<?php echo $options['header_logo']; ?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
+                        </a>
+                    </h1>
+                </hgroup>
+                <!-- #site-logo -->
             </div>
-            <div class="middle-header-content">
-                <?php travelify_socialnetworks($flag); ?>
-                <?php get_search_form(); ?>
-            </div>
-            <div class="menu-header-bottom">
-                <?php
-                $defaults = array(
-                    'menu'            => '78',
-                    'container'       => 'div',
-                    'container_class' => 'header_bottom',
-                    'container_id'    => '',
-                    'menu_class'      => 'menu',
-                    'menu_id'         => '',
-                    'echo'            => true,
-                );
-                wp_nav_menu($defaults);
-                ?>
+            <div class="hgroup-wrap-right">
+                <div class="menu-header-top">
+                    <?php
+                    $defaults = array(
+                        'menu'            => '77',
+                        'container'       => 'div',
+                        'container_class' => 'header_upper',
+                        'container_id'    => '',
+                        'menu_class'      => 'menu',
+                        'menu_id'         => '',
+                        'echo'            => true,
+                    );
+                    wp_nav_menu($defaults);
+                    ?>
+                </div>
+                <div class="middle-header-content">
+                    <?php travelify_socialnetworks($flag); ?>
+                    <?php get_search_form(); ?>
+                </div>
+                <div class="menu-header-bottom">
+                    <?php
+                    $defaults = array(
+                        'menu'            => '78',
+                        'container'       => 'div',
+                        'container_class' => 'header_bottom',
+                        'container_id'    => '',
+                        'menu_class'      => 'menu',
+                        'menu_id'         => '',
+                        'echo'            => true,
+                    );
+                    wp_nav_menu($defaults);
+                    ?>
+                </div>
             </div>
         </div>
     </div>
+
+    <?php
+    $args = array(
+        'theme_location' => 'primary',
+        'container' => '',
+        'items_wrap' => '<ul class="root">%3$s</ul>'
+    );
+    ?>
+
+    <nav id="main-nav" class="clearfix">
+        <div class="container clearfix">
+            <?php  wp_nav_menu($args); ?>
+        </div><!-- .container -->
+    </nav><!-- #main-nav -->
+    <div class="mobile-menu"></div>
 </div>
-
-<?php
-$args = array(
-    'theme_location' => 'primary',
-    'container' => '',
-    'items_wrap' => '<ul class="root">%3$s</ul>'
-);
-?>
-
-<nav id="main-nav" class="clearfix">
-    <div class="container clearfix">
-        <?php  wp_nav_menu($args); ?>
-    </div><!-- .container -->
-</nav><!-- #main-nav -->
-<div class="mobile-menu"></div>
