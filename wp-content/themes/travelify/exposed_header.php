80b4a6fe520d1a3b5ec797a8a7bd3bfb680617aa
Adrián A. Morelos Henriquez <adrian.morelos@akendos.com>
2015-10-13 15:18:13 -0500

    wp_exposed_header_callback

--- wp-content/themes/travelify/exposed_header.php
+++ wp-content/themes/travelify/exposed_header.php
@@ -3,8 +3,56 @@
  * Displays the header section of the theme.
  *
  */
-    print " <p>It works2!</p>";
+global $travelify_theme_options_settings;
+$options = $travelify_theme_options_settings;
+
+$elements = array();
+$elements = array(
+    $options['social_facebook'],
+    $options['social_twitter'],
+    $options['social_googleplus'],
+    $options['social_linkedin'],
+    $options['social_pinterest'],
+    $options['social_youtube'],
+    $options['social_vimeo'],
+    $options['social_flickr'],
+    $options['social_tumblr'],
+    $options['social_instagram'],
+    $options['social_rss'],
+    $options['social_github']
+);
+
+$flag = 0;
+if (!empty($elements)) {
+    foreach ($elements as $option) {
+        if (!empty($option)) {
+            $flag = 1;
+        } else {
+            $flag = 0;
+        }
+        if (1 == $flag) {
+            break;
+        }
+    }
+}
+
 ?>
-<div id="main" class="clearfix">
-    <p>It works2!</p>
+
+<div class="container clearfix">
+    <div class="hgroup-wrap clearfix">
+        <div class="hgroup-wrap-left">
+            <!-- #site-logo -->
+            <hgroup id="site-logo" class="clearfix">
+                <h1 id="site-title">
+                    <a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home">
+                        <img src="<?php echo $options['header_logo']; ?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
+                    </a>
+                </h1>
+            </hgroup>
+            <!-- #site-logo -->
+        </div>
+        <div class="hgroup-wrap-right">
+
+        </div>
+    </div>
 </div>
