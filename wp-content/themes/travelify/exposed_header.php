566e733addbfa2beb447805a3f8b0367c2b59308
Adrián A. Morelos Henriquez <adrian.morelos@akendos.com>
2015-10-13 15:30:09 -0500

    wp_exposed_header_callback

--- wp-content/themes/travelify/exposed_header.php
+++ wp-content/themes/travelify/exposed_header.php
@@ -52,7 +52,53 @@ if (!empty($elements)) {
             <!-- #site-logo -->
         </div>
         <div class="hgroup-wrap-right">
-
+            <div class="menu-header-top">
+                <?php
+                $defaults = array(
+                    'menu'            => '77',
+                    'container'       => 'div',
+                    'container_class' => 'header_upper',
+                    'container_id'    => '',
+                    'menu_class'      => 'menu',
+                    'menu_id'         => '',
+                    'echo'            => true,
+                );
+                wp_nav_menu($defaults);
+                ?>
+            </div>
+            <div class="middle-header-content">
+                <?php travelify_socialnetworks($flag); ?>
+                <?php get_search_form(); ?>
+            </div>
+            <div class="menu-header-bottom">
+                <?php
+                $defaults = array(
+                    'menu'            => '78',
+                    'container'       => 'div',
+                    'container_class' => 'header_bottom',
+                    'container_id'    => '',
+                    'menu_class'      => 'menu',
+                    'menu_id'         => '',
+                    'echo'            => true,
+                );
+                wp_nav_menu($defaults);
+                ?>
+            </div>
         </div>
     </div>
 </div>
+
+<?php
+$args = array(
+    'theme_location' => 'primary',
+    'container' => '',
+    'items_wrap' => '<ul class="root">%3$s</ul>'
+);
+?>
+
+<nav id="main-nav" class="clearfix">
+    <div class="container clearfix">
+        <?php  wp_nav_menu($args); ?>
+    </div><!-- .container -->
+</nav><!-- #main-nav -->
+<div class="mobile-menu"></div>
