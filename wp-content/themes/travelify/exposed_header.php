521c2521f5b0452b406a5c9de1a7bfc6ec113c32
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-10-27 12:03:52 -0500

    They want this to say CHEF'S GADGETS but there is not enough room for it...are we able to squeeze the spacing a bit to make it work?

--- wp-content/themes/travelify/exposed_header.php
+++ wp-content/themes/travelify/exposed_header.php
@@ -133,10 +133,12 @@ if (!empty($elements)) {
 
     .exposed-header #main-nav ul.root li.menu-item-847 a {
         color: #ef3d42;
+        font-weight: bold;
     }
 
     .exposed-header #main-nav ul.root li.menu-item-847 a:hover {
         color: #fff;
+        font-weight: normal;
     }
 
     @media only screen and (min-width: 320px) and (max-width: 549px){
