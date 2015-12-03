b0bd08b26e419aab28a10ba5434dd2b4ce6cacef
Sebastian <sebasgd@gmail.com>
2015-12-03 18:13:22 -0500

    Fix price issue.

--- wp-content/themes/travelify/functions.php
+++ wp-content/themes/travelify/functions.php
@@ -122,9 +122,9 @@ add_filter('woocommerce_get_price_html', 'custom_price', 10, 2);
 
 function custom_price( $price, $product ) {
 	
-	if (array_key_exists('sale_price', $product)) {
-		if ($product['sale_price'] == $product['regular_price']) {
-			$price = '<span class="amount">Price: $' . $product['regular_price'] . '</span>';
+	if (property_exists('sale_price', $product)) {
+		if ($product->sale_price == $product->regular_price) {
+			$price = '<span class="amount">Price: $' . $product->regular_price . '</span>';
 		}
 	}
 
