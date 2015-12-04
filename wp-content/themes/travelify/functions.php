7f0734d7438c82940f6308a6aaf47c08860a35e5
Sebastian <sebasgd@gmail.com>
2015-12-04 12:35:33 -0500

    Cart menu item.

--- wp-content/themes/travelify/functions.php
+++ wp-content/themes/travelify/functions.php
@@ -119,13 +119,25 @@ endif; // travelify_setup
 /* Too late to implement a child theme */
 /* Adding this here againts proper programming practices */
 add_filter('woocommerce_get_price_html', 'custom_price', 10, 2);
+add_filter('wp_nav_menu_header_upper_items','add_woocommerce_cart_nav_item');
 
 function custom_price( $price, $product ) {
 	
 	if ($product->regular_price == $product->price) {
 		$price = '<span class="amount">$' . $product->regular_price . '</span>';
 	}
+	/*else {
+		
+	}*/
+	print_r($price);
 
 	return $price;
 }
+
+function add_woocommerce_cart_nav_item($items) {
+	global $woocommerce;
+ 
+	return $items .= '<li class="menu-item-cart-item"><a class="cart-contents" href="'.$woocommerce->cart->get_cart_url().'" title="View your shopping cart"> CART('. $woocommerce->cart->cart_contents_count . ') </a></li>';
+}
+
 ?>
