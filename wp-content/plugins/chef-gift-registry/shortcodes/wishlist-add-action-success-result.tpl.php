610ce47b71710db81658c11893371e6b218f5f75
Adrián Andres Morelos Henriquez <ing.adrianmorelos@gmail.com>
2016-06-15 16:36:46 -0500

    Ajustes

--- wp-content/plugins/chef-gift-registry/shortcodes/wishlist-add-action-success-result.tpl.php
+++ wp-content/plugins/chef-gift-registry/shortcodes/wishlist-add-action-success-result.tpl.php
@@ -1,4 +1,6 @@
-<p class="wishlist_p"><?php echo _e( "Your new wishlist was created.\n\n", 'ignitewoo-wishlists-pro' ); ?></p>
-<p class="wishlist_p">
-<?php echo _e( "The URL is :", 'ignitewoo-wishlists-pro' ); ?> <a heref="<?php echo get_permalink( $post_id ); ?>"><?php echo $wishlist_title; ?></a>	
-</p>
+<div class="note note-success">
+	<p class="wishlist_p"><?php echo _e( "Your new wishlist was created.\n\n", 'ignitewoo-wishlists-pro' ); ?></p>
+	<p class="wishlist_p">
+	<?php echo _e( "The URL is :", 'ignitewoo-wishlists-pro' ); ?> <a href="<?php echo get_permalink( $post_id ); ?>"><?php echo $wishlist_title; ?></a>	
+	</p>
+</div>
