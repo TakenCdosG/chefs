57d80491d06c5be7ccb679f7d97d506e816e0281
Carlos Leaño <carlos.anime@akendos.com>
2016-06-17 07:57:20 -0500

    modified password strength for registration

--- wp-content/themes/travelify/library/functions/functions.php
+++ wp-content/themes/travelify/library/functions/functions.php
@@ -859,4 +859,8 @@ if (!function_exists('travelify_posted_on')) :
     }
 
 endif;
+    function reduce_woocommerce_min_strength_requirement( $strength ) {
+        return 1;
+    }
+    add_filter( 'woocommerce_min_password_strength', 'reduce_woocommerce_min_strength_requirement' );
 ?>
