c827c779c8dd3195f401c6d152e0377273e6ea22
Adrián Andres Morelos Henriquez <ing.adrianmorelos@gmail.com>
2016-06-24 19:45:14 -0500

    Ajustes

--- wp-content/themes/travelify/woocommerce/content-single-product.php
+++ wp-content/themes/travelify/woocommerce/content-single-product.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	 }
 ?>
 
-<div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" <?php post_class(array('row')); ?>>
    <div class="col-md-6">
 		<?php
 			/**
