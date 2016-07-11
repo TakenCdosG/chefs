c40088a715a63005a028010f1d583cd92082bf57
Carlos Leaño <carlos.anime@akendos.com>
2016-07-11 14:32:54 -0500

    fixed products image

--- wp-content/themes/travelify/woocommerce/content-product.php
+++ wp-content/themes/travelify/woocommerce/content-product.php
@@ -45,7 +45,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 		 * @hooked woocommerce_template_loop_product_thumbnail - 10
 		 */
 		?>
-		<div style="height:250px;">
+		<div style="height:155px;">
 		<?php
 			do_action( 'woocommerce_before_shop_loop_item_title' );
 		?>
