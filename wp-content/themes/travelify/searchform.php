622c58632c47002e8758fc13216545e18380d36a
Adrián A. Morelos Henriquez <adrian.morelos@akendos.com>
2016-07-13 10:30:21 -0400

    Ajustes!.

--- wp-content/themes/travelify/searchform.php
+++ wp-content/themes/travelify/searchform.php
@@ -3,8 +3,8 @@
  * Displays the searchform of the theme.
  */
 ?>
-	<form action="<?php echo esc_url( home_url( '/' ) ); ?>" class="searchform" method="get">
-		<label class="assistive-text" for="s"><?php _e( 'Search', 'travelify' ); ?></label>
-		<input type="text" placeholder="<?php esc_attr_e( 'Search', 'travelify' ); ?>" class="s field" name="s">
-        <input type="submit" class="field-submit .btn-search" value="<?php echo get_search_query(); ?>">
-	</form>
+<form action="<?php echo esc_url( home_url( '/' ) ); ?>" class="searchform" method="get">
+  <label class="assistive-text" for="s"><?php _e( 'Search', 'travelify' ); ?></label>
+  <input type="text" placeholder="<?php esc_attr_e( 'Search', 'travelify' ); ?>" class="s field" name="s">
+  <input type="submit" class="field-submit .btn-search" value="<?php echo get_search_query(); ?>">
+</form>
