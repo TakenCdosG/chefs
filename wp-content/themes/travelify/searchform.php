3a5e35c6dcc290af191fc5c6502d71ecf5c60981
Adrián A. Morelos Henriquez <adrian.morelos@akendos.com>
2016-07-13 10:30:03 -0400

    Ajustes!.

--- wp-content/themes/travelify/searchform.php
+++ wp-content/themes/travelify/searchform.php
@@ -6,5 +6,5 @@
 	<form action="<?php echo esc_url( home_url( '/' ) ); ?>" class="searchform" method="get">
 		<label class="assistive-text" for="s"><?php _e( 'Search', 'travelify' ); ?></label>
 		<input type="text" placeholder="<?php esc_attr_e( 'Search', 'travelify' ); ?>" class="s field" name="s">
-        <input type="submit" class="field-submit .btn-search" value="<?php echo get_query_var("s"); ?>">
+        <input type="submit" class="field-submit .btn-search" value="<?php echo get_search_query(); ?>">
 	</form>
