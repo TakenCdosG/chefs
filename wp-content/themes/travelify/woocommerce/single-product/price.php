4cac105b3f179140048ffb5ab6c7c20ae60b8258
Adrián A. Morelos Henriquez <adrian.morelos@akendos.com>
2016-07-13 01:03:18 -0400

    Ajustes!.

--- wp-content/themes/travelify/woocommerce/single-product/price.php
+++ wp-content/themes/travelify/woocommerce/single-product/price.php
@@ -16,19 +16,17 @@
  * @version 2.4.9
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+if (!defined('ABSPATH')) {
+  exit; // Exit if accessed directly
 }
 
 global $product;
 
 ?>
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-
-	<p class="price"><?php echo $product->get_price_html(); ?></p>
-
-	<meta itemprop="price" content="<?php echo esc_attr( $product->get_display_price() ); ?>" />
-	<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
-	<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
-
+  <p class="price"><?php echo $product->get_price_html(); ?></p>
+  <p class="sku"><span class="custom-price">Sku: <?php echo $product->get_sku(); ?></span></p>
+  <meta itemprop="price" content="<?php echo esc_attr($product->get_display_price()); ?>"/>
+  <meta itemprop="priceCurrency" content="<?php echo esc_attr(get_woocommerce_currency()); ?>"/>
+  <link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>"/>
 </div>
