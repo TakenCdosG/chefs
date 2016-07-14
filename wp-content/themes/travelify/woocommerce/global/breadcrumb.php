f26ebbd825da7cc72e83eebbef27da7b0517d710
Adrián A. Morelos Henriquez <adrian.morelos@akendos.com>
2016-07-14 13:12:43 -0400

    Ajustes!.

--- wp-content/themes/travelify/woocommerce/global/breadcrumb.php
+++ wp-content/themes/travelify/woocommerce/global/breadcrumb.php
@@ -10,38 +10,65 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woothemes.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
+ * @see      https://docs.woothemes.com/document/template-structure/
+ * @author    WooThemes
+ * @package  WooCommerce/Templates
  * @version     2.3.0
  * @see         woocommerce_breadcrumb()
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+  exit;
 }
 
 global $product;
-$defaults = array('fields' => 'ids');
-$args = wp_parse_args(  array(), $defaults );
-$result = wp_get_object_terms(array($product->id), 'product_cat');
-$category = reset($result);
-// dpm($category);
+if (isset($product->id)) {
+  $defaults = array('fields' => 'ids');
+  $args = wp_parse_args(array(), $defaults);
+  $result = wp_get_object_terms(array($product->id), 'product_cat');
+  $category = reset($result);
 
-if ( ! empty( $breadcrumb ) ) {
+  if (!empty($breadcrumb)) {
 
-	echo $wrap_before;
-  echo $before;
-  echo '<a href="' . esc_url( home_url( '/' ) ) . '">' . esc_html( "Home" ) . '</a>';
-  echo $after;
-  echo $delimiter;
-  echo $before;
-  echo '<a href="' . esc_url( home_url( $category->slug.'/') ) . '">' . esc_html( $category->name ) . '</a>';
-  echo $after;
-  echo $delimiter;
-  echo $before;
-  echo esc_html( $product->get_formatted_name());
-  echo $after;
-	echo $wrap_after;
+    echo $wrap_before;
+    echo $before;
+    echo '<a href="' . esc_url(home_url('/')) . '">' . esc_html("Home") . '</a>';
+    echo $after;
+    echo $delimiter;
+    echo $before;
+    echo '<a href="' . esc_url(home_url($category->slug . '/')) . '">' . esc_html($category->name) . '</a>';
+    echo $after;
+    echo $delimiter;
+    echo $before;
+    echo esc_html($product->get_title());
+    echo $after;
+    echo $wrap_after;
 
+  }
+} else {
+  if (!empty($breadcrumb)) {
+
+    echo $wrap_before;
+
+    foreach ($breadcrumb as $key => $crumb) {
+
+      echo $before;
+
+      if (!empty($crumb[1]) && sizeof($breadcrumb) !== $key + 1) {
+        echo '<a href="' . esc_url($crumb[1]) . '">' . esc_html($crumb[0]) . '</a>';
+      } else {
+        echo esc_html($crumb[0]);
+      }
+
+      echo $after;
+
+      if (sizeof($breadcrumb) !== $key + 1) {
+        echo $delimiter;
+      }
+
+    }
+
+    echo $wrap_after;
+
+  }
 }
