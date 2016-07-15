5519194f5896156fb5eafa6cc916a64b512a9875
Adrián A. Morelos Henriquez <adrian.morelos@akendos.com>
2016-07-15 14:09:40 -0400

    Ajustes

--- wp-content/themes/travelify/woocommerce/content-product.php
+++ wp-content/themes/travelify/woocommerce/content-product.php
@@ -45,7 +45,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 		 * @hooked woocommerce_template_loop_product_thumbnail - 10
 		 */
 		?>
-		<div style="height:155px;display: block;position: relative;">
+		<div style="height:155px;display: block;position: relative;padding: 8px;">
 		<?php
 			do_action( 'woocommerce_before_shop_loop_item_title' );
 		?>
